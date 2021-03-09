9cf6d428cbc26721e88e7a9dcfc94e9bd8a03981
Danil <dinkodanil@gmail.com>
2021-03-09 10:50:13 +0300

    Add Open API Swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors()
+
+  const config = new DocumentBuilder()
+    .setTitle('CRUD API')
+    .setDescription('This is crud api )))0')
+    .setVersion('1.0')
+    .addTag('crud')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
 
   await app.listen(3000);
 }
